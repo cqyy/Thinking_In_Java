2d02c369b3ca7daef0d09ab2fbf66611785bf753
dyyy <527068423@qq.com>
2014-02-25 21:59:08 +0800

    red-black-tree

---
+++ src/cn/yuanye/datastructure/RBTreeNode.java
@@ -0,0 +1,20 @@
+package cn.yuanye.datastructure;
+
+
+/**
+ * Created by Administrator on 14-2-19.
+ * Node of red-black tree
+ */
+class RBTreeNode {
+
+    public RBTreeNode(NodeColor color){
+        this.color = color;
+    }
+
+    public NodeColor color;
+    public RBTreeNode p;         // parent of the node
+    public RBTreeNode left;      // left subtree of the node
+    public RBTreeNode right;     // right subtree of the node
+
+    public int value;            //value of the node
+}
