f5fec297f03e129fa90b37ee03e7aa1c0af05e98
dyyy <527068423@qq.com>
2014-05-08 12:15:45 +0800

    2014-5-8

---
+++ src/cn/yuanye/nio/BufferCreation.java
@@ -0,0 +1,33 @@
+package cn.yuanye.nio;
+
+import java.nio.CharBuffer;
+
+/**
+ * Created by Kali on 14-5-6.
+ * This example focus on creation of buffer.
+ * This example use CharBuffer to illustrate the method of creation.
+ */
+public class BufferCreation {
+    public static void main(String... args){
+        //allocate
+        //directly allocate a buffer to hold 1024 char object.
+        CharBuffer buffer1 = CharBuffer.allocate(1024);
+
+        //wrap
+        //wrap will not create a space to hold the elements, the buffer will just use the array object
+        //to hold the elements,so the change is visible to both the array and the buffer
+        char[] str = "Buffer".toCharArray();
+        CharBuffer buffer2 = CharBuffer.wrap(str);
+        System.out.println(buffer2);
+        str[0] = 'b';                           //change the array object
+        System.out.println(buffer2);
+
+        //wrap with offset will just affects the initial state of the buffer object.
+        char[] str2 = "Hello Buffer".toCharArray();
+        CharBuffer buffer3 = CharBuffer.wrap(str2,6,6);
+        System.out.println(buffer3);
+        buffer3.clear();
+        System.out.println(buffer3);
+
+    }
+}
