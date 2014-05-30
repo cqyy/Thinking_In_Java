72084b7449b2a2fbf99452d218ed7d1df24c4929
yy <527068423@qq.com>
2014-05-30 17:27:39 +0800

    basic classloaser

--- java/src/main/java/Temp.java
+++ java/src/main/java/Temp.java
@@ -1,26 +1,9 @@
-import java.nio.ByteBuffer;
-
 /**
  * Created by Kali on 14-5-13.
  */
 public class Temp {
-
-    private static class Class1{
-        private final String str;
-
-        private Class1(String str) {
-            this.str = str;
-        }
-    }
-
-    public void printName(){
-        System.out.println(this.getClass().getName());
-    }
-
-
-    public static void main(String[] args){
-        ByteBuffer buffer = ByteBuffer.allocate(32);
-        buffer.flip();
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class clazz = Class.forName("Temp",false,Temp.class.getClassLoader().getParent());
     }
 }
 
